f6f3f9b53b31687dff0536c80a6a26668fc01191
Johan Steven Ospina Arroyave <33870427+stevenarroyave@users.noreply.github.com>
2017-11-28 14:16:44 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,29 +7,17 @@ function setup() {
 }
 
 function draw() {
-  background(0);
 
-  var mouse1 = map(mouseX, 0, width, 0, 255);
-  var mouse2 = map(mouseY, 0, height, 0, 255);
-  var mouse3 = map(mouseX, 0, width, 255, 0);
-  var mouse4 = map(mouseY, width, 255, 0);
-
-
-  var color1 = color(255,0,0, mouse1);
-  var color2 = color(0,255,0, mouse2);
-  var color3 = color(255,255,0, mouse3);
-  var color4 = color(0,0,255, mouse4);
-
-    fill(color1);
+    fill(255,0,0);
     rect(0, 0, 250, 250);    //Red rectangle
 
-    fill(color2);
+    fill(0,255,0);
     rect(0, 250, 250, 250);    //Green rectangle
 
-    fill(color3);
+    fill(255,255,0);
     rect(250, 0, 250, 250);  //yellow rectangle
 
-    fill(color4);
+    fill(0,0,255);
     rect(250, 250, 250, 250);   //blue rectangle
 
     if (mouseX < 250 && mouseY < 250) {
